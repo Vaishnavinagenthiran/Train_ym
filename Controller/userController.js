01a0aff8ef95d08256bf2163e289831dd7294ae5
Vaishnavi <vaishnavinagenthiran@gmail.com>
2026-01-29 11:56:50 +0530

    crud

--- Controller/userController.js
+++ Controller/userController.js
@@ -13,3 +13,47 @@ export const createUserController = async (req,res) => {
         res.status(500).json({ error : err.message});
     }
 }
+
+// get users 
+export const getAllUserController = async (req,res) => {
+    try{
+        const data = await UserModel.getAllUsersModel();
+        res.json(data)
+    }catch(err){
+        res.status(500).json({error : err.message})
+    }
+
+    }
+
+    export const updateUserPasswordController = async (req,res) => {
+        try { 
+            const {password }= req.body;
+            const updatePassword = await UserModel.updateUserPasswordModel(req.params.id,
+            { password});
+            if(!updatePassword){
+                res.status(404).json({ message: "user not found"});
+            }
+            else{
+                res.json({
+                    message: "password has been update"
+                })
+            }
+        } catch (err){
+            res.status(500).json({ error: err.message})
+        }
+    }
+    export const deleteUserController = async (req,res) => {
+        try { 
+            const delte = await UserModel.deleteUserModel(req.params.id);
+            if(!delte){
+                res.status(404).json({ message: "user not found"});
+            }
+            else{
+                res.json({
+                    message: "password has been removed from ur table"
+                })
+            }
+        } catch (err){
+            res.status(500).json({ error: err.message})
+        }
+    }
---
+++ Routes/userRoute.js
@@ -0,0 +1,13 @@
+import {createUserController, getAllUserController, updateUserPasswordController,deleteUserController } from "../Controller/userController.js";
+
+import express from 'express';
+const userRoute = express.Router(); // get,put,post,delete
+
+userRoute.post('/signup', createUserController); 
+userRoute.get('/getusers', getAllUserController);
+userRoute.put('/updatepass/:id', updateUserPasswordController);
+userRoute.delete('/deleteuser/:id', deleteUserController);
+
+// http://localhost:5000/api/user/signup
+
+export default userRoute;
